2d22bf36c85d4d235aceab35d3f701a4312f51d8
Matheus Ferreira <matheusfer.dev@gmail.com>
2023-09-19 19:17:18 -0300

    aplicando o react-router

--- src/store/reducers/filtro.ts
+++ src/store/reducers/filtro.ts
@@ -1,18 +1,31 @@
-// import { createSlice } from '@reduxjs/toolkit'
-// import * as enums from '../../utils/enums/Tarefa'
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import * as enums from '../../utils/enums/Tarefa'
 
-// type FiltroSlice = {
-//   termo: string
-//   criterio: 'prioridade' | 'status' | 'todas'
-//   valor?: enums.Prioridade | enums.Status
-// }
+type FiltroState = {
+  termo?: string
+  criterio: 'prioridade' | 'status' | 'todas'
+  valor?: enums.Prioridade | enums.Status
+}
 
-// const initialState: filtroState = {
-//   termo: '',
-//   criterio:
-// }
+const initialState: FiltroState = {
+  termo: '',
+  criterio: 'todas'
+}
 
-// const filtroSlice = createSlice({
-//   name: 'filtro',
-//   initialState,
-// })
+const filtroSlice = createSlice({
+  name: 'filtro',
+  initialState,
+  reducers: {
+    alterarTermo: (state, action: PayloadAction<string>) => {
+      state.termo = action.payload
+    },
+    alterarFiltro: (state, action: PayloadAction<FiltroState>) => {
+      state.criterio = action.payload.criterio
+      state.valor = action.payload.valor
+    }
+  }
+})
+
+export const { alterarTermo, alterarFiltro } = filtroSlice.actions
+
+export default filtroSlice.reducer
